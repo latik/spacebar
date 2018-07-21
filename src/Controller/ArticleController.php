f886a2ceba9edcdc7094a3c6f08970b76348ad7d
Latik <latkovskyi@gmail.com>
2018-07-21 14:55:34 +0300

    new show article page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -23,7 +23,7 @@ class ArticleController extends AbstractController
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug, LoggerInterface $logger, Twig $twig)
     {
