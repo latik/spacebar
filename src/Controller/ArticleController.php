83307552d4aacfcaab7aef544d188846b544f9bf
Latik <latkovskyi@gmail.com>
2018-07-22 18:41:56 +0300

    use Article entity mapping

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,9 +5,7 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use App\Service\SlackClient;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -30,19 +28,10 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show(
-      $slug,
-      SlackClient $slack,
-      EntityManagerInterface $em
-    ) {
+    public function show(Article $article, SlackClient $slack)
+    {
         $slack->sendMessage('Kahn', 'Ah, Kirk, my old friend...');
 
-        $repository = $em->getRepository(Article::class);
-        /** @var Article $article */
-        $article = $repository->findOneBy(['slug' => $slug]);
-        if (!$article) {
-            throw $this->createNotFoundException(sprintf('No article for slug "%s"', $slug));
-        }
         $comments = [
           'first' => '1 comment',
           'second' => '2 comment',
