9b5dae6fc30c9bd5733bba21b3f44ba7415ac88e
Latik <latkovskyi@gmail.com>
2018-07-28 23:41:38 +0300

    resolve Tags-Articles relation N+1 issue

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -26,6 +26,8 @@ class ArticleRepository extends ServiceEntityRepository
           ->addCriteria(CommentRepository::createNonDeletedCriteria());
 
         return $this->addIsPublishedQueryBuilder()
+          ->leftJoin('a.tags', 't')
+          ->addSelect('t')
           ->orderBy('a.publishedAt', 'DESC')
           ->getQuery()
           ->getResult();
