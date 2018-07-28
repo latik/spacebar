332401b06f440fc164559630fe82e52fdca9582d
Latik <latkovskyi@gmail.com>
2018-07-28 22:01:54 +0300

    add search for admin comments listing

--- src/Controller/AdminCommentController.php
+++ src/Controller/AdminCommentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\CommentRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminCommentController extends Controller
@@ -13,10 +14,15 @@ class AdminCommentController extends Controller
      * @Route("/admin/comment", name="admin_comment")
      * @Template
      */
-    public function index(CommentRepository $repository)
+    public function index(CommentRepository $repository, Request $request)
     {
+        $query = $request->query->get('q');
+
+        $comments = $repository->findAllWithSearch($query);
+
         return [
-          'comments' => $repository->findBy([], ['createdAt' => 'DESC']),
+          'comments' => $comments,
+          'query' => $query,
         ];
     }
 }
