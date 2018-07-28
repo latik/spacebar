e83a1f439dd7c6a393f786e85fa7084813106524
Latik <latkovskyi@gmail.com>
2018-07-28 13:29:36 +0300

    add Timestampable

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -52,6 +52,16 @@ class Article
      */
     private $imageFilename;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
     public function getId()
     {
         return $this->id;
@@ -149,6 +159,30 @@ class Article
     public function incrementHeartCount(): self
     {
         $this->heartCount++;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
