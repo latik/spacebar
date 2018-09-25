5fa7fe10b63a0204aba58191c3f1dcdf1d17f4ac
Latik <latkovskyi@gmail.com>
2018-09-25 13:52:57 +0300

    store last email due auth process

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
@@ -41,7 +42,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
      */
     protected function getLoginUrl()
     {
-        // TODO: Implement getLoginUrl() method.
+        return $this->router->generate('app_login');
     }
 
     /**
@@ -85,10 +86,17 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
      */
     public function getCredentials(Request $request)
     {
-        return [
+        $credentials = [
           'email' => $request->request->get('email'),
           'password' => $request->request->get('password'),
         ];
+
+        $request->getSession()->set(
+          Security::LAST_USERNAME,
+          $credentials['email']
+        );
+
+        return $credentials;
     }
 
     /**
