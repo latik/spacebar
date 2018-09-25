f0fae12254a1d437f8f900b09005a62b7192c642
Latik <latkovskyi@gmail.com>
2018-09-25 12:23:35 +0300

    mailer

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
