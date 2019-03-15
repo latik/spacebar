5af488350f9fb4ab4a0653d8b9b4caee59cc6704
Latik <latkovskyi@gmail.com>
2019-03-15 13:47:58 +0200

    Add EmailAddress and Password ValueObjects

---
+++ src/ValueObject/Password.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ValueObject;
+use InvalidArgumentException;
+use const PASSWORD_DEFAULT;
+
+final class Password
+{
+    /**
+     * @var string
+     */
+    private $password;
+
+    private function __construct()
+    {
+    }
+
+    public static function fromString(string $string): self
+    {
+        if ('' === $string) {
+            throw new InvalidArgumentException('An empty password is not acceptable');
+        }
+
+        $instance = new self();
+
+        $instance->password = $string;
+
+        return $instance;
+    }
+
+    public function toHash(): string
+    {
+        return password_hash($this->password, PASSWORD_DEFAULT);
+    }
+
+    public function matches(string $hash): bool
+    {
+        return password_verify($this->password, $hash);
+    }
+}
