95c0739d8e778aa6e6fe8c444c4bb19f9e903fee
Alexey Latkovskyi <latkovskyi@gmail.com>
2019-06-10 00:15:17 +0300

    bump versions

--- config/bundles.php
+++ config/bundles.php
@@ -20,5 +20,4 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
