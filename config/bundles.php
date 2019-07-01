2d15bcc787de489ae96746441ece224a197a4178
Alexey Latkovskyi <latkovskyi@gmail.com>
2019-07-01 09:56:39 +0300

    use messenger-enqueue-transport

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
+    Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
+    Enqueue\MessengerAdapter\Bundle\EnqueueAdapterBundle::class => ['all' => true],
 ];
