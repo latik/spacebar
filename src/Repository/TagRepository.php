917862a03b83f8d138cd051beea350ecb02fbbe1
Alexey Latkovskyi <latkovskyi@gmail.com>
2019-11-23 01:46:59 +0200

    fix deprecated

--- src/Repository/TagRepository.php
+++ src/Repository/TagRepository.php
@@ -6,7 +6,7 @@ namespace App\Repository;
 
 use App\Entity\Tag;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 
 /**
  * @method null|Tag find($id, $lockMode = null, $lockVersion = null)
@@ -16,7 +16,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 final class TagRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Tag::class);
     }
